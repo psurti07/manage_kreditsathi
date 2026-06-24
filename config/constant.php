e7f68833ab7816dc014f4a4d777e88f97817620b
vw-uday <verloop.dev4@gmail.com>
2026-06-24 18:08:19 +0530

    code add

---
+++ config/constant.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'master_api_key' => 'SWRNR0dvcFpBRDE0WjhMRHFnZUNwWHlReStxTlZlMXBkZi9WZHdSbHVpST0=',
+    'company_code'   => 'QIKBRW5678',
+    'local_ip'       => '190.92.174.183',
+];
